2c5de7a763bcc41549905d58d520f173d9ce6f5a
Jacilyn Hayden <jacihayden@gmail.com>
2018-08-26 03:31:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,19 @@ function distanceTravelledInFeet(start, destination) {
 // gives cust. free sample for 400 ft, $0.02/ft b/w 400 - 2000 ft, $25 for distance > 2000 ft, does not allow rides > 2500 ft
 function calculatesFarePrice(start, destination) {
   distance = distanceTravelledInFeet(start, destination);
-  if 
+  if (distance <= 400) {
+    farePrice = 0
+  }
+  else if (distance <= 2000) {
+    farePrice = (distance - 400) * 0.02
+  }
+  else if (distance <= 2500) {
+    farePrice = 25
+  }
+  else {
+    farePrice = 
+  }
+  return farePrice
 }
 
 
